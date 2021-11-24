68ab920cc519d7cebc4ab10ff8c12ca0b1f6b6e6
Shoaib <59787578+shoaibrain@users.noreply.github.com>
2021-11-23 19:43:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.secret_key = 'many random bytes'
+app.secret_key = 'gps'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
